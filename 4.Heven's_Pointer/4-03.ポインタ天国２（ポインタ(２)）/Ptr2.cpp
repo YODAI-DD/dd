fb6885db5f7a0433dff2413a6f43c498108effa4
dd <r1.38takasi@gmail.com>
2026-02-08 13:34:03 +0900

    yolo

---
+++ 4.Heven's_Pointer/4-03.ポインタ天国２（ポインタ(２)）/Ptr2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int  a;
+	int  b;
+	int* p = &a;
+
+	a = 0;
+	b = a;
+	cout << " a = " <<  a << endl
+		 << " b = " <<  b << endl
+		 << "*p = " << *p << endl;
+
+	int* p = &b;
+
+	*p = 5;
+	 b = a;
+	cout << " a = " <<  a << endl
+		 << " b = " <<  b << endl
+		 << "*p = " << *p << endl;
+}
---
+++ 4.Heven's_Pointer/4-07.配列とポインタの切っても切れない関係（アドレスの演算(２)）/StrLen2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+size_t StrLen(const char* str)
+{
+	const char* p;
+	for (p = str; *p != '\0'; ++p)
+	{
+		cout << p << endl;
+	}
+	return p - str;
+}
+
+void ShowLength(const char* str)
+{
+	cout << "文字列「" << str << "」の長さは"
+		<< StrLen(str) << "バイトです。" << endl;
+}
+
+int main()
+{
+	ShowLength("Hello");
+}
