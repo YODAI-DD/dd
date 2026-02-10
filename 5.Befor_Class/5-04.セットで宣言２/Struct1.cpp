3b9c5edd1c01d4529d96d02d94b6e418777143b7
dd <r1.38takasi@gmail.com>
2026-02-11 03:22:14 +0900

    enumまで

---
+++ 5.Befor_Class/5-04.セットで宣言２/Struct1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+const int MAX_NAME = 16;
+
+struct Student
+{
+	char name[MAX_NAME + 1];
+	int scoreJapanese;
+	int scoreMath;
+	int scoreEnglish;
+};
+
+void Show(const Student& stu)
+{
+	cout << "名前   : " << stu.name          << endl
+		 << "  国語 : " << stu.scoreJapanese << " 点"
+		 << ", 数学 : " << stu.scoreMath     << " 点"
+		 << ", 英語 : " << stu.scoreEnglish  << " 点" << endl;
+}
+
+int main()
+{
+	Student student[] =
+	{
+		{ "山口真子" , 73, 98, 86, },
+		{ "勝村摩耶" , 64, 45, 40, },
+		{ "新井彩花" , 76, 78, 69, },
+	};
+
+	int size = sizeof student / sizeof * student;
+
+	for (int i = 0; i < size; ++i)
+	{
+		Show(student[i]);
+	}
+}
---
+++ 5.Befor_Class/5-05.セットで宣言３（構造体）/Struct1b.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+
+const int MAX_NAME = 50;
+
+struct Student
+{
+	char name[MAX_NAME + 1];
+	int scoreJapanese;
+	int scoreMath;
+	int scoreEnglish;
+};
+
+void Show(const Student* pointer)
+{
+	cout << "名前 : " << pointer->name << endl
+		<< "国語 : " << pointer->scoreJapanese << " 点"
+		<< ", 数学 : " << pointer->scoreMath << " 点"
+		<< ", 英語 : " << pointer->scoreEnglish << " 点" << endl;
+}
+
+int main()
+{
+	Student student[] =
+	{
+		{"高橋陽大", 73, 98, 86, },
+		{"山口真子", 80, 99, 97, },
+		{"ヒルマンニナ", 69, 88, 100, },
+	};
+	int size = sizeof student / sizeof * student;
+
+	for (int i = 0; i < size; ++i)
+	{
+		Show(&student[i]);
+	}
+}
