3ff6580ab92fa1c4b7f6aca6002224cc22bcb07b
dd <r1.38takasi@gmail.com>
2026-02-12 01:03:32 +0900

    リンケージまで

---
+++ 5.Befor_Class/5-11.インドア派 vs アウトドア派/Extern1.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+
+int a = 2;  //aの実体
+
+void Func() //Funcの実体
+{
+	cout << "a = " << a << endl;
+}
---
+++ 5.Befor_Class/5-11.インドア派 vs アウトドア派/Internal1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+static int a = 2;
+
+static void Func1()
+{
+	cout << "a = " << a << endl;
+}
+
+void Func2()
+{
+	Func1();
+}
