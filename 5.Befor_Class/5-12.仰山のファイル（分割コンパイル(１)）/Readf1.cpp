4c749472e3da64f6899987ff8a4daec8876c0124
dd <r1.38takasi@gmail.com>
2026-02-14 00:28:42 +0900

    配列のポインタ

---
+++ 5.Befor_Class/5-12.仰山のファイル（分割コンパイル(１)）/Readf1.cpp
@@ -0,0 +1,12 @@
+﻿#include "Readf1.h"
+#include "Readf1.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << N << endl;
+}
+
+//ヘッダファイルには関数プロトタイプを入れておくとよい。
+//また、二重インクルード防止のために、#pragma onceを入れておくとよい。
