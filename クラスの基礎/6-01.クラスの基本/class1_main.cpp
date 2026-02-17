b4877b03822616e2992cae7711e3a81c8ec60992
デムデム <r1.38takasi@gmail.com>
2026-02-18 01:11:44 +0900

    クラスの基礎（メンバ関数とは？）

---
+++ クラスの基礎/6-01.クラスの基本/class1_main.cpp
@@ -0,0 +1,19 @@
+﻿#include "class1_class.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	Student student[] =
+	{
+		{"高橋陽大", 73, 98, 86, },
+		{"山口真子", 80, 99, 97, },
+		{"ヒルマンニナ", 69, 88, 100, },
+	};
+	int size = sizeof student / sizeof * student;
+
+	for (int i = 0; i < size; ++i)
+	{
+		Show(&student[i]);
+	}
+}
